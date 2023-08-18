296744ead7af61a379df805ded048cfd9b2a6498
stevenTheDeve <steventhedeve@gmail.com>
2023-08-17 21:33:35 -0700

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Estevan Hinojosa on 8/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack (spacing: 0){
+                Color.red
+                Color.cyan
+                
+            }
+            Text("UPHILL")
+                .font(.custom("San Francisco", size: 70))
+                .foregroundStyle(.secondary)
+                .fontWeight(.heavy)
+                .padding(20)
+                .background(.ultraThinMaterial)
+                    
+                    
+            
+                    
+            Text("RADIO")
+                .font(.subheadline)
+                .fontWeight(.bold)
+                .foregroundColor(Color.yellow)
+                .frame(width: 120, height: 44, alignment: .bottomTrailing)
+            }
+            .padding()
+            .ignoresSafeArea()
+    }
+        
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
